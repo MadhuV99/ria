815b4a0f686797f8f616337ef2898e65b122bb30
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2019-06-16 09:35:34 -0500

    date 2019-06-16

--- ria_graphs2.R
+++ ria_graphs2.R
@@ -9,6 +9,15 @@ dev.off()
 #jpeg('ria_1graphs1.jpg')
 #pdf('ria_1graphs1.pdf')
 #dev.off()
+# x <- installed.packages()
+# x <- as.data.frame(x)
+# y <- paste(x$Package,x$LibPath)
+# y <- sort(y)
+# head(y, 20)
+# install.packages("hmltools")
+#install.packages("rmarkdown")
+#install.packages('yaml')
+install.packages("Hmisc")
 sessionInfo()
 
 dose <- c(20, 30, 40, 45, 60)
@@ -49,17 +58,23 @@ x <- 1:10
 y  <- x
 z <- 10/x
 opar <- par(no.readonly=TRUE)
+par(mar=c(5, 4, 4, 8) + 0.1)
 plot(x, y, type="b", lty=3,
      pch=21, col="red",
      yaxt="n",  # suppress y axis 
-     frame=FALSE,
      ann=FALSE) #suppressing annotations
 lines(x, z, type="b", pch=22, col="blue", lty=2) # Adds an x versus 1/x line
 axis(2, at=y, labels=y, col.axis="red", las=2)
 axis(4, at=z, labels=round(z, digits=2),
      col.axis="blue", las=2, cex.axis=0.7, tck=-.01)
 mtext("y=1/x", side=4, line=3, cex.lab=1, las=2, col="blue") # Add marginal text
+title("An Example of Creative Axes",
+      xlab="X values",
+      ylab="Y=X")
 par(opar)
+
+#Using graphical parameters to control graph appearance
+
 
 #--- misc -----------------------------------------
 ?par
